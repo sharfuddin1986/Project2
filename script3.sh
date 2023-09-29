5a7e57ff1f197fe88ba9d24359e8be98b7ba849f
md sharffuddin <106474548+sharfuddin1986@users.noreply.github.com>
2023-09-29 23:32:17 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,1 +1,10 @@
-
+sudo apt update -y
+sudo apt install docker.io -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl apt-transport-https ca-certificates software-properties-common
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo add-apt-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo swapoff -a
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
