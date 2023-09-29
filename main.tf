5a52dff22f6e4d943ba040422b7e92404d037b80
md sharffuddin <106474548+sharfuddin1986@users.noreply.github.com>
2023-09-29 19:00:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,32 @@
 provider "aws" {
-    region = "us-east-2"
-    access_key = "your_Acess_key"
-    secret_key = "your_Secret_key"
-    }
-resource "aws_instance" "K8s-master" {
-    ami = "ami-024e6efaf93d85776"
-    instance_type = "t2.medium"
-    key_name  =  "NV"
-    tags  =  {
-      Name  =  "Machine-3"
-    }
-} 
-resource "aws_instance" "K8s-Slave-1" {
-    ami = "ami-024e6efaf93d85776"
-    instance_type = "t2.micro"
-    key_name  =  "NV"
-    tags  =  {
-      Name  =  "Machine-2"
-    }
-} 
-resource "aws_instance" "K8s-Slave-2" {
-    ami = "ami-024e6efaf93d85776"
-    instance_type = "t2.micro"
-    key_name  =  "NV"
-    tags  =  {
-      Name  =  "Machine-4"
-    }
-} 
+  region  = "us-east-1"
+  access_key  = "acess-key"
+  secret_key  = "secret-key"
+}
 
+resource "aws_instance" "K8s-Master" {
+  ami = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.medium"
+  key_name  = "NV"
+  tags  = {
+    key_name  = "Machine-3"
+  }
+}
+
+resource "aws_instance" "K8s-Slave-1" {
+  ami = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.micro"
+  key_name  = "NV"
+  tags  = {
+    key_name  = "Machine-2"
+  }
+}
+
+resource "aws_instance" "K8s-Slave-2" {
+  ami = "ami-0261755bbcb8c4a84"
+  instance_type = "t2.micro"
+  key_name  = "NV"
+  tags  = {
+    key_name  = "Machine-4"
+  }
+}
